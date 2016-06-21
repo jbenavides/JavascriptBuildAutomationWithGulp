3e6575215d26d5c3e7c4d8058a748dc152a9c96c
Jose Benavides <jbenavides.bermudez@gmail.com>
2016-06-21 00:17:04 -0500

    CSS compilation

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,16 @@
 module.exports = function(){
-    var config = {
 
+    var client = './src/client/';
+    var config = {
+        temp: './.tmp',
+        
         //all js to vet
         alljs: [
                 './src/**/*.js',
            	    './*.js'
-        ]
+        ],
+
+        less: client + 'styles/styles.less'
     };
 
     return config;
